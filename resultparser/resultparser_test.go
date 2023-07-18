f16fd971892bb67b8a159486dacce0e3d20f12ad
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-07-18 15:44:33 +0200

    test(resultparser): rm wrong permission test case

--- resultparser/resultparser_test.go
+++ resultparser/resultparser_test.go
@@ -13,16 +13,10 @@ func TestWriteResultsToCSV(t *testing.T) {
 	tempInputFile := "/tmp/benchmark.log"
 	defer os.Remove(tempInputFile)
 
-	wrongPermissionsFile := "/tmp/wrong_permissions.log"
-	defer os.Remove(tempInputFile)
-
 	cleanData := `HPL_AI 1 1 1 1 0.001 10.0 1 1 9.5
 	HPL_AI 2 2 2 2 0.002 20.0 1 1 19.0`
-	permissionData := `HPL_AI 1 1 1 1 0.001 10.0 1 1 9.5
-	HPL_AI 2 2 2 2 0.002 20.0 1 1 20`
+
 	err := os.WriteFile(tempInputFile, []byte(cleanData), 0644)
-	require.NoError(t, err)
-	err = os.WriteFile(wrongPermissionsFile, []byte(permissionData), 0000)
 	require.NoError(t, err)
 
 	tests := []struct {
@@ -35,11 +29,7 @@ func TestWriteResultsToCSV(t *testing.T) {
 			input:   "/tmp/benchmark.log",
 			wantErr: false,
 		},
-		{
-			name:    "Incorrect permission",
-			input:   "/tmp/wrong_permissions.log",
-			wantErr: false,
-		},
+
 		{
 			name:    "File does not exist",
 			input:   "/tmp/non_existing_file.txt",
